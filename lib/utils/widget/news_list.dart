f8d6d6ed018656d450d1447148d15e89a4536018
YamiSagi <84624853+yamisagi@users.noreply.github.com>
2022-09-27 18:18:43 +0300

    fixed Exception bug

--- lib/utils/widget/news_list.dart
+++ lib/utils/widget/news_list.dart
@@ -1,7 +1,9 @@
 import 'dart:convert';
 
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:news_app/model/news_model.dart';
+import 'package:news_app/utils/constants.dart';
 import 'package:news_app/utils/widget/shimmer.dart';
 import 'package:news_app/utils/widget/touch_area.dart';
 
@@ -19,20 +21,35 @@ class NewsList extends StatelessWidget {
           child: FutureBuilder<String>(
             future: future,
             builder: (BuildContext context, AsyncSnapshot<String> snapshot) {
-              if (snapshot.hasData) {
-                final NewsModel newsModel =
-                    NewsModel.fromJson(jsonDecode(snapshot.data!));
-                return ListView.builder(
-                  itemCount: newsModel.articles?.length ?? 0,
-                  itemBuilder: (BuildContext context, int index) {
-                    var article = newsModel.articles![index];
-                    return TouchArea(article: article);
-                  },
+              try {
+                if (snapshot.hasData) {
+                  final NewsModel newsModel =
+                      NewsModel.fromJson(jsonDecode(snapshot.data!));
+                  return ListView.builder(
+                    itemCount: newsModel.articles?.length ?? 0,
+                    itemBuilder: (BuildContext context, int index) {
+                      var article = newsModel.articles![index];
+                      return TouchArea(article: article);
+                    },
+                  );
+                } else if (snapshot.hasError) {
+                  return const Text('Error');
+                } else {
+                  return const Shimmer();
+                }
+              } on Exception {
+                return Container(
+                  width: MediaQuery.of(context).size.width * 0.9,
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  decoration: BoxDecoration(
+                    borderRadius: Constant.smallPicRadius,
+                    image: const DecorationImage(
+                      image: CachedNetworkImageProvider(Constant.notFound,
+                          scale: 1),
+                      fit: BoxFit.scaleDown,
+                    ),
+                  ),
                 );
-              } else if (snapshot.hasError) {
-                return const Text('Error');
-              } else {
-                return const Shimmer();
               }
             },
           ),
